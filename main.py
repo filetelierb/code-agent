598f1e33967df5c1b967792f67190f60c10b664b
feletelier <felipe@igniuslab.com>
2025-09-02 17:56:43 +0200

    Added --verbose flag

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 if len(sys.argv) > 1:
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
+    
+    verbose = "--verbose" in sys.argv
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-lite",contents=sys.argv[1])
+    response = client.models.generate_content(model="gemini-2.0-flash-lite",contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 else:
     print("Please provide an argument to run the script.")
     exit(1)
